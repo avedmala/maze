d62532fd12e4f69b99f20348f842afd537864f8c
Abhinav <abhinav.vedmala@gmail.com>
2019-09-24 19:22:45 -0400

    created location class

---
+++ Location.java
@@ -0,0 +1,26 @@
+public class Location {
+
+  private int x;
+  private int y;
+
+  public Location(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setX(int newX) {
+    x = newX;
+  }
+
+  public void setY(int newY) {
+    y = newY;
+  }
+}
